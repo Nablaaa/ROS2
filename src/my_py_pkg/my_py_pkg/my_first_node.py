29d9885c5ad75c0bcea5b7c6cac6dde0fc1f30db
Nablaaa <Eric_Schmidt_99@gmx.de>
2024-12-28 15:56:33 +0100

    add callback and timer

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -6,6 +6,12 @@ class MyCustomNode(Node):
     def __init__(self):
         super().__init__('my_node_name')
         self.get_logger().info("Hello World!")
+        self.counter_ = 0
+        self.timer_ = self.create_timer(1.0, self.timer_callback) 
+
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info('Counter: {0}'.format(self.counter_))
 
 
 def main(args=None):
