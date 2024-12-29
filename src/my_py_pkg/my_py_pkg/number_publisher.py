ee59850dbafe3f70210eed3fdec052af28929f85
Nablaaa <Eric_Schmidt_99@gmx.de>
2024-12-29 21:17:09 +0100

    packages are renamed-updated

--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import Int64 # here i import an interface
+from example_interfaces.msg import Int64, Float64 # here i import an interface
 
 class NumberPublisherNode(Node):
     def __init__(self):
@@ -9,6 +9,7 @@ class NumberPublisherNode(Node):
         
         self.number = 42
         self.number_publisher = self.create_publisher(Int64, 'number', 10)
+        self.float_publisher = self.create_publisher(Float64, 'number_float', 10)
 
         self.timer = self.create_timer(0.5, self.publish_number)
         self.get_logger().info("publishing starts")
@@ -17,6 +18,10 @@ class NumberPublisherNode(Node):
         msg = Int64()
         msg.data = self.number
         self.number_publisher.publish(msg)
+
+        msg_float = Float64()
+        msg_float.data = self.number + 0.1234
+        self.float_publisher.publish(msg_float)
 
 def main(args=None):
     rclpy.init(args=args)
