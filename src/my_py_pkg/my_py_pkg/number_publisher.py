25c956d920f92235799ae3f7a96eaf8ec97c04ab
Nablaaa <Eric_Schmidt_99@gmx.de>
2025-01-01 18:23:06 +0100

    use custom interface for publishing

--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64, Float64 # here i import an interface
+from my_robot_interfaces.msg import HardwareStatus
 
 class NumberPublisherNode(Node):
     def __init__(self):
@@ -13,8 +14,15 @@ class NumberPublisherNode(Node):
         self.number_publisher = self.create_publisher(Int64, 'number', 10) 
         self.float_publisher = self.create_publisher(Float64, 'number_float', 10)
 
+        self.hardware_status_publisher = self.create_publisher(HardwareStatus, 'hardware_status', 10)
+
         self.timer = self.create_timer(0.5, self.publish_number)
+        
+        self.hardware_timer = self.create_timer(1.0, self.publish_hardware_status)
+        
         self.get_logger().info("publishing starts")
+
+
 
     def publish_number(self):
         msg = Int64()
@@ -24,6 +32,17 @@ class NumberPublisherNode(Node):
         msg_float = Float64()
         msg_float.data = self.number + 0.1234
         self.float_publisher.publish(msg_float)
+
+
+    def publish_hardware_status(self):
+        msg = HardwareStatus()
+        msg.version = 1
+        msg.temperature = 32.1
+        msg.are_motors_ready = True
+        msg.debug_message = "All is fine"
+        self.hardware_status_publisher.publish(msg)
+
+
 
 def main(args=None):
     rclpy.init(args=args)
