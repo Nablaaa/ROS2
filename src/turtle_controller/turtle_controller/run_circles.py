8c541f3ac10a70786bbfee76d6ea2a00ced41f67
Nablaaa <Eric_Schmidt_99@gmx.de>
2025-01-09 08:06:14 +0100

    give pen a color depending on position

--- src/turtle_controller/turtle_controller/run_circles.py
+++ src/turtle_controller/turtle_controller/run_circles.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose, Color
+from turtlesim.srv import SetPen
 
 
 
@@ -21,7 +22,16 @@ class RunCircles(Node):
         # subscriber pose
         self.sub_position = self.create_subscription(Pose, 'turtle1/pose', self.ControlTurtle, 10)
 
-    def ControlTurtle(self, msg):
+        self.client = self.create_client(SetPen, 'turtle1/set_pen')
+
+
+
+
+    def ControlTurtle(self, msg: Pose):
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().warn('service not available, waiting again...')
+        
+
         # print position
         self.get_logger().info('Position: x={0}, y={1}, theta={2}'.format(msg.x, msg.y, msg.theta))
 
@@ -32,11 +42,32 @@ class RunCircles(Node):
         vel_msg.linear.x = 1.0
         vel_msg.angular.z = 1.0
 
+        request = SetPen.Request()
+        request.r = 0
+        request.g = 255
+        request.b = 0
+        request.width = 2
+
         if x > self.screen_center_x:
             vel_msg.linear.x = 2.0
             vel_msg.angular.z = 2.0
 
+
+            request.r = 255
+            request.g = 255
+            request.b = 255
+            request.width = 5
+
+        future = self.client.call_async(request)
+        future.add_done_callback(self.callback_set_pen)
+
         self.pub_velocity.publish(vel_msg)
+
+    def callback_set_pen(self, future):
+        try:
+            response = future.result()
+        except Exception as e:
+            self.get_logger().error(f"Service call failed {str(e)}")
 
 
 
