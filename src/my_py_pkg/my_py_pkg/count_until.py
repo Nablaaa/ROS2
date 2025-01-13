eae99894cac5bc7b0858ea444492c96609d5d5ce
Nablaaa <Eric_Schmidt_99@gmx.de>
2025-01-13 12:44:03 +0100

    finish action server

--- src/my_py_pkg/my_py_pkg/count_until.py
+++ src/my_py_pkg/my_py_pkg/count_until.py
@@ -4,7 +4,7 @@ from rclpy.node import Node
 from rclpy.action import ActionServer
 from rclpy.action import GoalResponse # to send feedback
 from rclpy.action.server import ServerGoalHandle # used when action is accepted
-from my_robot_interfaces.action import CountUntil
+from my_robot_interfaces.action import CountUntil # has CountUntil.Goal, .Feedback, .Result
 
 
 class CountUntilServer(Node):
@@ -28,7 +28,33 @@ class CountUntilServer(Node):
         return GoalResponse.ACCEPT
     
     def execute_callback(self, goal_handle: ServerGoalHandle):
+        # get GOAL request from client
         target_number = goal_handle.request.target_number
         delay = goal_handle.request.delay
-        result = CountUntil.Result()
+
+
         counter = 0
+
+        self.get_logger().info(f'Start execution. Counting until {target_number}')
+        for _ in range(target_number):
+            counter += 1
+            self.get_logger().info(str(counter))
+            time.sleep(delay) # this is just here to simulate that an action needs time (different to a server)
+
+        # once goal is reached
+        goal_handle.succeed() # aborted and canceled are other options
+
+        # prepare result
+        result = CountUntil.Result()
+        result.reached_number = counter
+        return result
+    
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CountUntilServer()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
